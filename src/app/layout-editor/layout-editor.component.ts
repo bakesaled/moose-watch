6df7059942bc972ac1bc3455b00c15c99f92de5d
Brandon Eleuterio <eleuterio@railroad19.com>
2017-12-18 22:32:57 -0700

    changing background of layout-editor

--- src/app/layout-editor/layout-editor.component.ts
+++ src/app/layout-editor/layout-editor.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'mw-layout-editor',
   templateUrl: './layout-editor.component.html',
-  styleUrls: ['./layout-editor.component.scss']
+  styleUrls: ['./layout-editor.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class LayoutEditorComponent implements OnInit {
-
+  @HostBinding('class.mw-layout-editor') layoutEditorClass = true;
   constructor() { }
 
   ngOnInit() {
